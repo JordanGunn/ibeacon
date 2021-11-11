5ea2ab8d6cc5331a023473a3ffcfc0ff587b3f64
ckvan42 <giwounbae@gmail.com>
2021-11-10 20:39:20 -0800

    Add store_data, fetch_data, delete_data function to database

--- include/db.h
+++ include/db.h
@@ -5,29 +5,51 @@
 #ifndef IBEACON_DB_H //what's this??
 #define IBEACON_DB_H
 
-#include <dc_posix/dc_netdb.h>
-#include <dc_posix/dc_posix_env.h>
+#include <dc_application/command_line.h>
+#include <dc_application/config.h>
+#include <dc_application/defaults.h>
+#include <dc_application/environment.h>
+#include <dc_application/options.h>
+#include <dc_posix/dc_stdlib.h>
+#include <dc_posix/dc_string.h>
+#include <dc_posix/dc_ndbm.h>
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 //Main purpose of this thing is: to handle HTTP requests, and responses.
 //This is to interact with DB.
 //use the ndbm.
 
-//possible methods: GET, PUT, POST http
+//possible methods: GET, PUT, POST, DELETE http
 //These stuff will be dealt with on server side.
 //After parsing of the HTTP message, it will know which method they are.
 
 //When it gets the GET:
     //It will call to search the db to grab.
 
-
 //use struct
 //struct parent {int type;}
-//struct ndbm.c
-    //struct ndbm_db {struct parent super;}
+//struct ndbm.c //struct ndbm_db {struct parent super;}
 
-typedef struct DB * DBptr;
+
+/**
+ * Stores the data to the database
+ *
+ * @param env
+ * @param err
+ * @param db DBM pointer
+ * @param key Major + Minor
+ * @param value GPS location and the time.
+ * @param type REPLACE or INSERT
+ * @return int value
+ */
+int store_data(const struct dc_posix_env *env, struct dc_error *err, DBM *db, const char *key, const char *value, uint8_t type);
+
+datum fetch_data(const struct dc_posix_env *env, struct dc_error *err, DBM *db, const char *name);
+
+int delete_data(const struct dc_posix_env *env, struct dc_error *err, DBM *db, const char *name);
 
 
 #endif //IBEACON_DB_H
--- src/db.c
+++ src/db.c
@@ -5,4 +5,37 @@
 #include "db.h"
 //Is going to be ndbm_db.c
 
+//Maybe we need to encapsulate the function calls.
+
+
+/*
+ * ATTENTION! The type will always be REPLACE.
+ * REPLACE will create the key if not exist, and UPDATE when exist.
+ */
+int store_data(const struct dc_posix_env *env, struct dc_error *err, DBM *db, const char *key_to_add, const char *value_to_add, uint8_t type)
+{
+    int ret_val;
+    datum key = {key_to_add, strlen(key_to_add) + 1}; //adding one as a \n
+    datum value = {value_to_add, strlen(value_to_add) + 1}; //adding one as a \n
+
+    ret_val = dc_dbm_store(env, err, db, key, value, type);
+    return ret_val; //returns 0 if successful, and negative value if fail.
+}
+
+datum fetch_data(const struct dc_posix_env *env, struct dc_error *err, DBM *db, const char *name)
+{
+    datum key = {(void*)name, strlen(name)};
+    datum content;
+    content = dc_dbm_fetch(env, err, db, key);
+    return content;
+}
+
+int delete_data(const struct dc_posix_env *env, struct dc_error *err, DBM *db, const char *name)
+{
+    datum key = {(void*)name, strlen(name)};
+    int ret_val;
+
+    ret_val = dc_dbm_delete(env, err, db, key);
+    return ret_val; //returns 0 if successful, and negative value if fail.
+}
 
